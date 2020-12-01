6d1763284e2e51da32941372a6a0934caa2414ee
Elvin Lee <39041751+elvinyhlee@users.noreply.github.com>
2020-12-01 13:40:59 +0800

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,24 +1,22 @@
-#1
-count = 0
+def part1(nums):
+    for i in nums:
+        for j in nums:
+            if i + j == 2020:
+                return i * j
+
+
+def part2(nums):
+    for i in nums:
+        for j in nums:
+            for k in nums:
+                if i + j + k == 2020:
+                    return i * j * k
+
+
 with open('day1-data.txt') as f:
-    for line in f:
-        num = int(line)
-        count += (num-num%3)//3 - 2
-
-f.close()
-print(count)
-
-
-
-#2
-count = 0
-with open('day1-data.txt') as f:
-    for line in f:
-        num = int(line)
-        num = (num-num%3)//3 - 2
-        while num > 0:
-            count += num
-            num = (num-num%3)//3 - 2
-
-f.close()
-print(count)
+    inputs = [
+        int(line)
+        for line in f
+    ]
+    print(part1(inputs))
+    print(part2(inputs))
