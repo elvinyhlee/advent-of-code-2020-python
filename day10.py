dcc86f34c22953c6d010ecde23807a4ad56a2d8f
Elvin Lee <39041751+elvinyhlee@users.noreply.github.com>
2020-12-10 13:47:42 +0800

    Update day10.py

--- day10.py
+++ day10.py
@@ -1,37 +1,29 @@
+from collections import Counter
+
+
 def part1(nums):
     last_jolt = 0
-    diff_1, diff_2, diff_3 = 0, 0, 0
+    diffs = [0, 0, 0]
 
     jolts = sorted(nums)
     jolts.append(jolts[-1] + 3)
 
     for jolt in jolts:
-        if jolt - last_jolt == 1:
-            diff_1 += 1
-        elif jolt - last_jolt == 2:
-            diff_2 += 1
-        elif jolt - last_jolt == 3:
-            diff_3 += 1
-
+        diffs[jolt - last_jolt - 1] += 1
         last_jolt = jolt
 
-    return diff_1 * diff_3
+    return diffs[0] * diffs[2]
 
 
 def part2(nums):
     jolts = sorted(nums)
     jolts.append(jolts[-1] + 3)
 
-    dp = {0: 1}
+    dp = Counter()
+    dp[0] = 1
 
     for jolt in jolts:
-        dp[jolt] = 0
-        if jolt - 1 in dp:
-            dp[jolt] += dp[jolt - 1]
-        if jolt - 2 in dp:
-            dp[jolt] += dp[jolt - 2]
-        if jolt - 3 in dp:
-            dp[jolt] += dp[jolt - 3]
+        dp[jolt] = dp[jolt - 1] + dp[jolt - 2] + dp[jolt - 3]
 
     return dp[jolts[-1]]
 
@@ -44,3 +36,4 @@ with open('day10-data.txt') as f:
 
     print(part1(inputs))
     print(part2(inputs))
+
