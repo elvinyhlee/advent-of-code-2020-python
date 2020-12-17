7021203f91caa6c9d70809ce29f1c703cb6a0218
Elvin Lee <39041751+elvinyhlee@users.noreply.github.com>
2020-12-18 01:24:33 +0800

    Update day17.py

--- day17.py
+++ day17.py
@@ -7,38 +7,48 @@ INACTIVE = '.'
 
 def part1(data):
     # Initialize grid
-    grid = [[['.' for _ in range(24)] for _ in range(24)] for _ in range(24)]
+    rows = len(data)
+    columns = len(data[0])
+    loops = 6
+    expanded = loops * 2
+
+    grid = [[['.' for _ in range(columns + expanded)] for _ in range(rows + expanded)] for _ in range(1 + expanded)]
     for ix, i in enumerate(data):
         for jx, j in enumerate(i):
-            grid[12][ix+9][jx+9] = j
+            grid[loops][ix + loops][jx + loops] = j
 
     # Run 6 cycles
-    for _ in range(6):
+    si = sj = sk = loops - 1  # s means start from
+
+    for loop in range(loops):
         grid_clone = copy.deepcopy(grid)
-        for i in range(24):
-            for j in range(24):
-                for k in range(24):
+
+        range_expanded = 2 * (loop + 1)
+        for i in range(1 + range_expanded):
+            for j in range(rows + range_expanded):
+                for k in range(columns + range_expanded):
                     count_active = 0
                     for z in itertools.product([0, 1, -1], repeat=3):
                         if not(z == (0, 0, 0))\
-                                and (0 <= i + z[0] <= 23) \
-                                and (0 <= j + z[1] <= 23) \
-                                and (0 <= k + z[2] <= 23) \
-                                and grid[i+z[0]][j+z[1]][k+z[2]] == ACTIVE:
+                                and (0 <= i + si + z[0] < 1 + expanded) \
+                                and (0 <= j + sj + z[1] < rows + expanded) \
+                                and (0 <= k + sk + z[2] < columns + expanded) \
+                                and (grid[i + si + z[0]][j + sj + z[1]][k + sk + z[2]] == ACTIVE):
                             count_active += 1
 
-                    if (grid[i][j][k] == ACTIVE) and (not(2 <= count_active <= 3)):
-                        grid_clone[i][j][k] = INACTIVE
+                    if (grid[i + si][j + sj][k + sk] == ACTIVE) and (not(2 <= count_active <= 3)):
+                        grid_clone[i + si][j + sj][k + sk] = INACTIVE
 
-                    if (grid[i][j][k] == INACTIVE) and (count_active == 3):
-                        grid_clone[i][j][k] = ACTIVE
+                    if (grid[i + si][j + sj][k + sk] == INACTIVE) and (count_active == 3):
+                        grid_clone[i + si][j + sj][k + sk] = ACTIVE
         grid = grid_clone
+        si, sj, sk = si - 1, sj - 1, sk - 1
 
     # Count active cubes
     active = 0
-    for i in range(24):
-        for j in range(24):
-            for k in range(24):
+    for i in range(1 + expanded):
+        for j in range(rows + expanded):
+            for k in range(columns + expanded):
                 if grid[i][j][k] == ACTIVE:
                     active += 1
     return active
@@ -46,41 +56,52 @@ def part1(data):
 
 def part2(data):
     # Initialize grid
-    grid = [[[['.' for _ in range(24)] for _ in range(24)] for _ in range(24)] for _ in range(24)]
+    rows = len(data)
+    columns = len(data[0])
+    loops = 6
+    expanded = loops * 2
+
+    grid = [[[['.' for _ in range(columns + expanded)] for _ in range(rows + expanded)]
+             for _ in range(1 + expanded)] for _ in range(1 + expanded)]
     for ix, i in enumerate(data):
         for jx, j in enumerate(i):
-            grid[12][12][ix+9][jx+9] = j
+            grid[loops][loops][ix + loops][jx + loops] = j
 
     # Run 6 cycles
-    for _ in range(6):
+    si = sj = sk = sl = loops - 1  # s means start from
+
+    for loop in range(loops):
         grid_clone = copy.deepcopy(grid)
-        for i in range(24):
-            for j in range(24):
-                for k in range(24):
-                    for l in range(24):
+
+        range_expanded = 2 * (loop + 1)
+        for i in range(1 + range_expanded):
+            for j in range(1 + range_expanded):
+                for k in range(rows + range_expanded):
+                    for l in range(columns + range_expanded):
                         count_active = 0
                         for z in itertools.product([0, 1, -1], repeat=4):
                             if not(z == (0, 0, 0, 0))\
-                                    and (0 <= i + z[0] <= 23) \
-                                    and (0 <= j + z[1] <= 23) \
-                                    and (0 <= k + z[2] <= 23) \
-                                    and (0 <= l + z[3] <= 23) \
-                                    and (grid[i+z[0]][j+z[1]][k+z[2]][l+z[3]] == ACTIVE):
+                                    and (0 <= i + si + z[0] < 1 + expanded) \
+                                    and (0 <= j + sj + z[1] < 1 + expanded) \
+                                    and (0 <= k + sk + z[2] < rows + expanded) \
+                                    and (0 <= l + sl + z[3] < columns + expanded) \
+                                    and (grid[i + si + z[0]][j + sj + z[1]][k + sk + z[2]][l + sl + z[3]] == ACTIVE):
                                 count_active += 1
 
-                        if (grid[i][j][k][l] == ACTIVE) and not(2 <= count_active <= 3):
-                            grid_clone[i][j][k][l] = INACTIVE
+                        if (grid[i + si][j + sj][k + sk][l + sl] == ACTIVE) and (not(2 <= count_active <= 3)):
+                            grid_clone[i + si][j + sj][k + sk][l + sl] = INACTIVE
 
-                        if (grid[i][j][k][l] == INACTIVE) and (count_active == 3):
-                            grid_clone[i][j][k][l] = ACTIVE
+                        if (grid[i + si][j + sj][k + sk][l + sl] == INACTIVE) and (count_active == 3):
+                            grid_clone[i + si][j + sj][k + sk][l + sl] = ACTIVE
         grid = grid_clone
+        si, sj, sk, sl = si - 1, sj - 1, sk - 1, sl - 1
 
     # Count active cubes
     active = 0
-    for i in range(24):
-        for j in range(24):
-            for k in range(24):
-                for l in range(24):
+    for i in range(1 + expanded):
+        for j in range(1 + expanded):
+            for k in range(rows + expanded):
+                for l in range(columns + expanded):
                     if grid[i][j][k][l] == ACTIVE:
                         active += 1
     return active
